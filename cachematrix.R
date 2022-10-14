7f9e976c3f9bfcf0e3fa51220bcc01322461aa64
yrnbyrng <114917616+yrnbyrng@users.noreply.github.com>
2022-10-13 22:25:49 -0600

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,72 +2,35 @@
 
 
 ## Creates a special matrix object that can cache its inverse
-makeCacheMatrix <- function(x = matrix() ) {
+makeCacheMatrix <- function(x = matrix()) {
   
-  ## Initialize the inverse property
-  Inv <- NULL
-  
-  ## Method to set the matrix
-  set <- function(matrix) {
-    x <<- matrix
+  Inv = NULL
+  set <- function(y) {
+    x <<- y
     Inv <<- NULL
   }
-  
-  ## The matrix
-  get <- function() {
-    ## Returns the matrix
-    x
+  get <- function() {x}
+  setInv <- function(i) {Inv <- i}
+  getInv <- function() {Inv}
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
   }
-  
-  ## Set the inverse of the matrix
-  setInv <- function(inverse) {
-    Inv <<- inverse
-  }
-  
-  ## Get the inverse of the matrix
-  getInv <- function() {
-    ## Return the inverse property
-    Inv
-  }
-  
-  ## Return a list of the methods
-  list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
-}
 
 
-
-## Compute the inverse of the special matrix returned by "makeCacheMatrix"
-## above. If the inverse has already been calculated (and the matrix has not
-## changed), then the "cachesolve" should retrieve the inverse from the cache.
+  
+##Compute the inverse of 'makeCacheMatrix'. The 'cachesolve' retrieves the 
+## inverse from the cache i.e. if the inverse is already calculated
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  x <- x$getInv()
   
-  ## Just return the inverse if its already set
-  if( !is.null(x) ) {
-    message("getting cached data")
-    return(x)
+  Inv = x$getInv()
+  
+  if(!is.null(Inv)) {
+    message("getting cached inverse")
+    return(Inv)
   }
-  
-  ## Get the matrix from our object
   A <- x$get()
-  
-  ## Use matrix multiplication to compute the inverse
-  x <- solve(A) %*% A
-  
-  ## Set the inverse to the object
-  x$setInv(x)
-  
-  ## Returns the matrix
-  x
+  Inv <- solve(A, ...)
+  x$setInv(Inv)
+  Inv
 }
-
-
-
-
-
-
-
